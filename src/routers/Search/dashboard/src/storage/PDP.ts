433cddabeffc825bb32e84d802284c54917c4e1b
kecso <tamas.kecskes@vanderbilt.edu>
2022-08-19 06:33:48 -0500

    Trying to add real data fetch from PDP to list artifacts

--- src/routers/Search/dashboard/src/storage/PDP.ts
+++ src/routers/Search/dashboard/src/storage/PDP.ts
@@ -1,15 +1,60 @@
 import testDataItems from "../TestResultData.js";
+import fetch from 'node-fetch';
 
 class PDP {
   token: string;
 
   constructor(token: string) {
+    console.log('this might be good');
     this.token = token;
   }
 
+  _getHeaders() {
+    return {
+      'Authorization': 'Bearer '+this.token,
+      'accept': 'application/json',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    };
+  }
+
+  async _getAllObservations(processId) {
+      const response = await fetch(
+        'https://leappremonitiondev.azurewebsites.net/v2/Process/GetProcessState?processId='+processId, {
+        method: 'get',
+        mode: 'no-cors', //TODO find a proper way, so far this appears to be the only thing that make the call at least reach the PDP
+        headers: this._getHeaders()
+      });
+      const obsInfo = response.json();
+      //TODO not sure how this environment can have promise
+      const observations = [];
+      for(let i=1;i<=obsInfo.numObservations; i+=1) {
+        const subResponse = await fetch(
+          'https://leappremonitiondev.azurewebsites.net/v2/Process/GetObservation?processId='+processId+'&obsIndex='+i, {
+            method: 'get',
+            mode: 'no-cors',
+            headers: this._getHeaders()
+          });
+        observations.push(await subResponse.json());
+      }
+      return observations;
+  }
+
   async listArtifacts() {
-    console.log('list artifacts');
-    return testDataItems;
+    const response = await fetch('https://leappremonitiondev.azurewebsites.net/v2/Process/ListProcesses?permission=read', {
+      method: 'get',
+      mode: 'no-cors',
+      headers: this._getHeaders()
+      });
+    const data : object[] = (await response.json() || [{}]).filter(element => element.processType === 'data');
+    let realDataItems = [];
+
+    data.forEach(process => {
+      const processObservations = await this._getAllObservations(process.processId);
+      realDataItems = realDataItems.concat(processObservations);
+    });
+    
+    return realDataItems;
   }
 
   async downloadArtifact(metadata) {
