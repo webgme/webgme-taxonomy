0382a00bf624a3ef78b61263f482cc19575381ae
Brian Broll <brian.broll@gmail.com>
2022-08-25 19:16:25 -0500

    Update formatting

--- src/common/TagCreatorForm.js
+++ src/common/TagCreatorForm.js
@@ -7,40 +7,49 @@ function factory() {
     }
 
     render(data) {
-
       const Form = JSONSchemaForm.default;
-      const {schema, uiSchema, formData, formatter} = data;
-      const children = React.createElement('div', null, [
+      const { schema, uiSchema, formData, formatter } = data;
+      const children = React.createElement("div", null, [
         ...data.buttons,
-          React.createElement('button', {
-              type: 'submit',
-              className: 'btn btn-secondary',
-              onClick: async () => {
-                  const downloadData = await this.formatData(formatter, formData);
-                  this.downloadJSON(downloadData);
-              },
-          }, 'Download'),
+        React.createElement(
+          "button",
+          {
+            type: "submit",
+            className: "btn btn-secondary",
+            onClick: async () => {
+              const downloadData = await this.formatData(formatter, formData);
+              this.downloadJSON(downloadData);
+            },
+          },
+          "Download"
+        ),
       ]);
-      this.root.render(React.createElement(Form, data.getProperties(), children));
+      this.root.render(
+        React.createElement(Form, data.getProperties(), children)
+      );
     }
 
-        async formatData(formatter, formData) {
-            formData.taxonomyTags = await formatter.getHumanFormat(formData.taxonomyTags);
-            return formData;
-        }
+    async formatData(formatter, formData) {
+      formData.taxonomyTags = await formatter.getHumanFormat(
+        formData.taxonomyTags
+      );
+      return formData;
+    }
 
-        downloadJSON(object, name = 'tags') {
-            const dataStr = "data:text/json;charset=utf-8," +
-                encodeURIComponent(JSON.stringify(object));
-            const element = document.createElement('a');
-            element.setAttribute("href", dataStr);
-            element.setAttribute("download", name + ".json");
-            document.body.appendChild(element);
-            element.click();
-            element.remove();
-        }
+    downloadJSON(object, name = "tags") {
+      const dataStr =
+        "data:text/json;charset=utf-8," +
+        encodeURIComponent(JSON.stringify(object));
+      const element = document.createElement("a");
+      element.setAttribute("href", dataStr);
+      element.setAttribute("download", name + ".json");
+      document.body.appendChild(element);
+      element.click();
+      element.remove();
+    }
 
-    static inject(_React, _ReactDOM, _JSONSchemaForm) {  // dependency injection
+    static inject(_React, _ReactDOM, _JSONSchemaForm) {
+      // dependency injection
       React = _React;
       ReactDOM = _ReactDOM;
       JSONSchemaForm = _JSONSchemaForm;
@@ -56,34 +65,45 @@ function factory() {
       this.buttons = [];
     }
 
-    addButton(name, action, clazz = 'btn-info') {
-          this.buttons.push(React.createElement('button', {
-              type: 'submit',
-              className: `btn ${clazz}`,
-              onClick: () => {
-                  action(this.formData);
-              },
-          }, name));
+    addButton(name, action, clazz = "btn-info") {
+      this.buttons.push(
+        React.createElement(
+          "button",
+          {
+            type: "submit",
+            className: `btn ${clazz}`,
+            onClick: () => {
+              action(this.formData);
+            },
+          },
+          name
+        )
+      );
     }
 
     getProperties() {
       let formData = this.formData;
-      const onChange = (event) => formData = event.formData;
-      return {schema: this.schema, onChange, uiSchema: this.uiSchema, formData};
+      const onChange = (event) => (formData = event.formData);
+      return {
+        schema: this.schema,
+        onChange,
+        uiSchema: this.uiSchema,
+        formData,
+      };
     }
   }
 
   const DefaultFormatter = {
-      getHumanFormat(tag) {
-          return tag;
-      }
+    getHumanFormat(tag) {
+      return tag;
+    },
   };
 
   TagCreatorForm.FormRenderData = FormRenderData;
   return TagCreatorForm;
 }
 
-if (typeof define !== 'undefined') {
+if (typeof define !== "undefined") {
   define([], factory);
 } else {
   this.TagCreatorForm = factory();
