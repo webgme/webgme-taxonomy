1499ee937090210d806cc25ad6f9a877bb26b6f6
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-02-09 15:56:14 -0600

    flesh out router Utils type definitions a tad

--- src/common/routers/Utils.d.ts
+++ src/common/routers/Utils.d.ts
@@ -1,8 +1,15 @@
+import type { Request, Response, Router, RequestHandler, NextFunction } from "express";
+import type { MiddlewareOptions } from "../types";
+
+type MiddlewareHandler<T extends boolean | void = boolean | void> =
+  (req: Request, res: Response, next: NextFunction) => Promise<T>;
+
 export class UserError extends Error {
   constructor(msg: string, code?: number);
   statusCode: number;
-  sendVia(response: any): void;
+  sendVia(response: Response): void;
 }
-export function handleUserErrors(logger: any, fn: any): (req: any, res: any, next: any) => Promise<void>;
-export function getProjectScopedRoutes(path: any): any;
-export function addProjectScopeMiddleware(middlewareOpts: any, router: any): void;
+
+export function handleUserErrors(logger: Global.GmeLogger, fn: MiddlewareHandler): MiddlewareHandler<void>;
+export function getProjectScopedRoutes(path: string): string[];
+export function addProjectScopeMiddleware(middlewareOpts: MiddlewareOptions, router: Router): Router;
