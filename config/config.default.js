b9bbd632e96bf766a9f83a7fd0f511177d45d97b
Brian Broll <brian.broll@gmail.com>
2023-04-19 08:44:32 -0500

    WIP adding tree-viz, transformation lib

--- config/config.default.js
+++ config/config.default.js
@@ -7,6 +7,19 @@ var config = require("./config.webgme"),
 config.server.port = +process.env.PORT || 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 
+const path = require("path");
+const jsonImporterDir = path.dirname(
+  require.resolve("webgme-json-importer/app.js"),
+);
+config.plugin.basePaths.push(path.join(jsonImporterDir, "src/plugins"));
+
+config.requirejsPaths["webgme-transformations"] = path.relative(
+  path.join(__dirname, ".."),
+  path.dirname(
+    require.resolve("webgme-transformations/package.json"),
+  ),
+);
+
 config.requirejsPaths.react =
   "./src/visualizers/widgets/TagCreator/lib/react.production.min",
   validateConfig(config);
