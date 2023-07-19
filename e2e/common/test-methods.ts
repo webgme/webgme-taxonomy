f7ba7f205b9669d7eb892b0720db4c320319f477
Dax <dax.westerman@vumc.org>
2023-07-19 17:52:57 -0500

    updated additional helper methods - candidates to move into fixture

--- e2e/common/test-methods.ts
+++ e2e/common/test-methods.ts
@@ -1,32 +1,147 @@
-import crypto from "crypto"
-
-// /**
-//  * Generator to create random project name for each iteration
-//  */
-// function* project_name_generator(prefix: string = "Project_"): Generator<string> {
-
-//     // Global hyphen search
-//     const HYPEN_RE: RegExp = /\-/gi;
-//     const REPLACEMENT_TOKEN: string = "_";
-
-//     let guid: string = crypto.randomUUID().toString()
-//     guid = guid.replace(HYPEN_RE, REPLACEMENT_TOKEN);
-//     const project_name: string = `${prefix}${guid}`;
-//     return project_name;
-// }
-
-// const PROJECT_NAMER = project_name_generator();
+import crypto from "crypto";
+import os from "os";
+import path from "path";
+// import * as fs from 'fs';
+import * as fs from "fs/promises";
 
 export default class TestMethods {
-    static * project_name_generator(prefix: string = "Project_"): Generator<string> {
+  /**
+   * Generator to create random project names based on a modified UUID
+   *
+   * @static
+   * @param {string} [prefix="Project_"]
+   * @return {*}  {Generator<string>}
+   * @memberof TestMethods
+   */
+  static *project_name_generator(
+    prefix: string = "Project_"
+  ): Generator<string> {
+    // Global hyphen search
+    const HYPEN_RE: RegExp = /\-/gi;
+    const REPLACEMENT_TOKEN: string = "_";
 
-        // Global hyphen search
-        const HYPEN_RE: RegExp = /\-/gi;
-        const REPLACEMENT_TOKEN: string = "_";
+    let guid: string = crypto.randomUUID().toString();
+    guid = guid.replace(HYPEN_RE, REPLACEMENT_TOKEN);
+    const project_name: string = `${prefix}${guid}`;
+    return project_name;
+  }
 
-        let guid: string = crypto.randomUUID().toString()
-        guid = guid.replace(HYPEN_RE, REPLACEMENT_TOKEN);
-        const project_name: string = `${prefix}${guid}`;
-        return project_name;
+  /**
+   * Create a temporary file in the local operating system of the form
+   * <name><cryptostring>.<extension>
+   *
+   * This is just a helper method, so no checking (e.g., validating the extension
+   * is more like 'csv' and not '.csv')
+   *
+   * Call the text_generator if supplied to populate the file
+   *
+   *
+   * @static
+   * @param {string} name
+   * @param {string} extension
+   * @param {TextGeneratorCall} text_generator
+   * @param {string} [temp_dir]
+   * @return {*}  {string}
+   * @memberof TestMethods
+   */
+  static async create_temp_file(
+    name: string,
+    extension: string,
+    text_generator?: () => Promise<string>,
+    temp_dir?: string
+  ): Promise<string> {
+    temp_dir = temp_dir ? temp_dir : os.tmpdir();
+    const temp_file_path = path.join(
+      temp_dir,
+      `${name}${crypto.randomBytes(16).toString("hex")}.${extension}`
+    );
+    if (text_generator != null) {
+      const generated_text = await text_generator();
+      await fs.writeFile(temp_file_path, generated_text);
     }
+    return temp_file_path;
+  }
+
+  /**
+   * Wrapper tp create a temporary taxonomy file in the system temp directory
+   *
+   * @static
+   * @return {*}  {string}
+   * @memberof TestMethods
+   */
+  static async create_temp_taxonomy_file(
+    taxonomy_generator: () => Promise<string> = TestMethods.taxonomy_generator_shim,
+    name: string = "taxonomy_file"
+  ): Promise<string> {
+    return TestMethods.create_temp_file(
+      "taxonomy_file",
+      "csv",
+      taxonomy_generator
+    );
+  }
+
+  static async taxonomy_generator_shim(): Promise<string> {
+    return [
+      "parentTerm,,,",
+      ",childTerm,,,",
+      ",,name (text),",
+      ",,age (int),",
+      ",,color (enum),",
+      ",,,red",
+      ",,,blue",
+      ",,,green",
+    ].join("\n");
+  }
+
+  static async setPageListener(pageref) {
+    await pageref.route(
+      new RegExp(".+socket.io.+$"),
+      async (route, request) => {
+        console.log(
+          `********** BEGIN Route - ${request.url()}\nHeaders: ${TestMethods.prettyPrintJson(
+            request.headers()
+          )}`
+        );
+        route.continue();
+        await request.response();
+        console.log(
+          `********** END Route - ${request.url()}\nHeaders: ${TestMethods.prettyPrintJson(
+            request.headers()
+          )}`
+        );
+      }
+    );
+  }
+
+  static prettyPrintJson(d) {
+    return JSON.stringify(d, null, 2);
+  }
+
+  static async generate_test_taxonomy_file() {
+    const TaxonomyParser = require("../src/common/TaxonomyParser");
+
+    const temp_taxonomy_file = await TestMethods.create_temp_taxonomy_file();
+
+    // Execute process to convert taxonomy from csv (./bin/taxonomy-from-csv <path-to-csv> > taxonomy.json)
+    const fs = require("fs");
+    const csvContent = fs.readFileSync(temp_taxonomy_file, "utf8");
+    const tags = TaxonomyParser.fromCSV(csvContent);
+
+    const taxonomy = {
+      pointers: {
+        base: "@meta:Taxonomy",
+      },
+      children: tags,
+    };
+
+    const temp_taxonomy_json_file = await TestMethods.create_temp_file(
+      "taxonomy",
+      "json",
+      async () => {
+        return JSON.stringify(taxonomy);
+      }
+    );
+
+    return temp_taxonomy_json_file;
+  }
 }
--- e2e/data-dashboard.spec.ts
+++ e2e/data-dashboard.spec.ts
@@ -1,78 +1,64 @@
 import { expect, request, test } from "@playwright/test";
 import TestMethods from "./common/test-methods";
+import { TestTaxonomy } from "./common/fixtures/TestTaxonomy";
+import { baseTest } from "./common/fixtures/taxonomy.fixtures";
 
-
-// TODO - Where to pull from config?
-const BASE_URL = "http://127.0.0.1:8080/";
-const TEST_SPEC_ISSUE_URL = "https://github.com/webgme/webgme-taxonomy/issues/304"
 const PROJECT_NAMER = TestMethods.project_name_generator();
-const PROJECT_NAME_SELECTOR = "body > div.ui-layout-north.no-print.ui-layout-pane.ui-layout-pane-north > div > div.navbar.navbar-inverse.navbar-fixed-top > div > div.project-navigator-controller.ng-scope > nav > ul > li:nth-child(4) > div.label-container.ng-scope > span";
-
-async function setPageListener(pageref) {
-  await pageref.route(new RegExp(".+socket\.io.+$"), async (route, request) => {
-    console.log(`********** BEGIN Route - ${request.url()}\nHeaders: ${prettyPrintJson(request.headers())}`);
-    route.continue();
-    await request.response();
-    console.log(`********** END Route - ${request.url()}\nHeaders: ${prettyPrintJson(request.headers())}`);
-  });
-}
-
-function prettyPrintJson(d) {
-  return JSON.stringify(d, null, 2);
-}
+const PROJECT_NAME_CSS_SELECTOR =
+  "body > div.ui-layout-north.no-print.ui-layout-pane.ui-layout-pane-north > div > div.navbar.navbar-inverse.navbar-fixed-top > div > div.project-navigator-controller.ng-scope > nav > ul > li:nth-child(4) > div.label-container.ng-scope > span";
 
 /**
  * Manual Change
- * 
- * Had to make a change within 
- * 
+ *
+ * Had to make a change within
+ *
  *    webgme-engine\src\server\storage\websocket.js
  *    line 884
- *  
+ *
  *    Abstracted cookie lookup to closure to permit looking up within lower calling scope
- * 
- *    const cookieRef = this.handshake.headers.cookie; 
- *    const cookies = () => parseCookie(cookieRef); 
- * 
+ *
+ *    const cookieRef = this.handshake.headers.cookie;
+ *    const cookies = () => parseCookie(cookieRef);
+ *
  * TODOs
- * 
- * 1) Need to figure out how to add handshake.headers.cookie to permit 
+ *
+ * 1) Need to figure out how to add handshake.headers.cookie to permit
  *    call in C:\_\wl\webgme_projects\webgme-taxonomy\node_modules\webgme-engine\src\server\storage\websocket.js
  *    line 885, socket.on('simpleRequest', function (data, callback)  {
  *              const cookies = await request.cookies();
  * 2) Inspect interface further to determine if can chain, or needs to re-query placeholder?
  * 3) Text name for placeholder seems fragile for hook; is there a more stable selector?
- * 
+ *
  * Questions
- * 
+ *
  *  1) Will there always be a static type of project to select?
  *  2) Should this grab the list of all projects and test for each one?
  *    * potential selector: div.create-new-project-dialog select.selector
- * 
+ *
  * Notes
- * 
+ *
  * Example of waiting for a selector to show up in the DOM
- *  
+ *
  *    await page.waitForSelector('div[class="modal-backdrop fade in"]');
- * 
+ *
  * Handling textbox typing detection logic
  *  - Need to dispatch 'keyup' event after fill, if validation check dependency.
- * 
+ *
  * Mechanism to check routes
- * 
+ *
  *    await page.route(new RegExp(".+socket\.io.+$"), async (route, request) => {
  *       console.log(`********** BEGIN Route - ${request.url()}\nHeaders: ${prettyPrintJson(request.headers())}`);
  *       route.continue();
  *       await request.response();
  *       console.log(`********** END Route - ${request.url()}\nHeaders: ${prettyPrintJson(request.headers())}`);
  *    })
- * 
- *    Can call 
- * 
- *    await setPageListener(page); 
- * 
+ *
+ *    Can call
+ *
+ *    await setPageListener(page);
+ *
  *    within code to monitor
- * 
+ *
  * Mechanism to check websocket calls
  *
  *    page.on('websocket', ws => {
@@ -87,15 +73,12 @@ function prettyPrintJson(d) {
  *
  */
 
-
 test.describe(`Data dashboard`, function () {
-
   test.beforeAll(async () => {
-    console.log(TEST_SPEC_ISSUE_URL)
-  })
+    console.log(`Issue: https://github.com/webgme/webgme-taxonomy/issues/304`);
+  });
 
-  test("Create new repo (and it shows up).", async ({ page, browser }) => {
-
+  test("Create new repo (and it shows up).", async ({ page }) => {
     const current_project_name: string = PROJECT_NAMER.next().value;
 
     // Routes to / based on baseUrl in config
@@ -116,19 +99,31 @@ test.describe(`Data dashboard`, function () {
     await page.getByRole("button", { name: "Create" }).click();
     await page.getByRole("combobox").selectOption("file:TaxonomyProject");
     await page.getByRole("button", { name: "Create" }).click();
-    await expect(page.locator(PROJECT_NAME_SELECTOR)).toHaveText(current_project_name, { timeout: 300000 });
+    await expect(page.locator(PROJECT_NAME_CSS_SELECTOR)).toHaveText(
+      current_project_name,
+      { timeout: 300000 }
+    );
 
+    await page.close();
   });
 
-  test.fixme("Upload new artifact to a repo and ensure it shows up", async ({ page }) => { });
+  baseTest(
+    "Upload new artifact to a repo and ensure it shows up",
+    async ({ page, test_taxonomy }) => {
+      // Begin navigation
+      await page.goto("/");
 
-  test.fixme("Download artifact", async ({ page }) => { });
+      const taxonomy = await test_taxonomy.get()
+      console.log(taxonomy)
+    }
+  );
 
-  test.fixme("View/download the metadata for the artifact (ensure correct)", async ({ page }) => { });
+  test.fixme("Download artifact", async ({ page }) => {});
 
-  test.fixme("Filter using:", async ({ page }) => { });
+  test.fixme(
+    "View/download the metadata for the artifact (ensure correct)",
+    async ({ page }) => {}
+  );
 
+  test.fixme("Filter using:", async ({ page }) => {});
 });
-
-
-
