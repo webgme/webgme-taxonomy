27e553cd9e9f9560fa5ccc496f7b6d5f02390d5a
Brian Broll <brian.broll@gmail.com>
2023-08-02 11:54:41 -0500

    Update list repo test (passing now)

--- e2e/data-dashboard.spec.ts
+++ e2e/data-dashboard.spec.ts
@@ -1,20 +1,49 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test.describe(`Data dashboard`, function () {
-  test.only("can create repo", async ({ page }) => {
+  test("can create repo", async ({ page }) => {
     const repoName = "NewExample-" + Date.now();
     await page.goto(
       "http://localhost:8080/routers/Search/guest%2Be2e_tests/branch/master/%2FC/static/",
     );
 
     // Create a new repo
-    // page.on("console", (msg) => console.log(msg.text()));
     await page.getByLabel(/Upload dataset/).click();
     await page.getByRole("textbox", { name: "Name" }).fill(repoName);
     await page.getByRole("button", { name: "Submit" }).click();
 
     // check that the new repository is there
+    // FIXME: there has to be a better way to do this...
     const element = page.getByTestId(repoName);
-    expect(await element.isVisible()).toBeTruthy();
+    const isVisible = await poll(() => element.isVisible(), { timeout: 3000 });
+    if (!isVisible) {
+      throw new Error(`Repository didn't show up after 3000 ms`);
+    }
   });
 });
+
+// TODO: it would be good to move these to a utils file or something
+interface PollOptions {
+  timeout?: number;
+  interval?: number;
+}
+
+async function poll(
+  fn: () => Promise<boolean>,
+  opts?: PollOptions,
+): Promise<boolean> {
+  const maxDuration = opts?.timeout || 1000;
+  const interval = opts?.interval || 10;
+
+  let duration = 0;
+  let isPassing = await fn();
+  while (!isPassing && duration < maxDuration) {
+    await sleep(interval);
+    duration += interval;
+    isPassing = await fn();
+  }
+  return isPassing;
+}
+async function sleep(ms: number): Promise<void> {
+  return new Promise((res) => setTimeout(res, ms));
+}
