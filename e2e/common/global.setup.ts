f56b6e952942e9c4e46d02238c7f9d52460f0d23
Dax <dax.westerman@vumc.org>
2023-08-04 16:57:04 -0500

    Removed redundant global for playwright

--- e2e/common/global.setup.ts
+++
@@ -1,7 +0,0 @@
-import { test as setup, expect, FullConfig } from '@playwright/test';
-
-async function globalSetup(config: FullConfig) {
-    console.log('setup..')
-}
-
-export default globalSetup;
