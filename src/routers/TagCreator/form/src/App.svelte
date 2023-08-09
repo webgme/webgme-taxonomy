90611a09cf0a50cbf0bd853c22786d942573d023
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-09 15:29:25 -0500

    add `SchemaForm` to TagCreator

--- src/routers/TagCreator/form/src/App.svelte
+++ src/routers/TagCreator/form/src/App.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
-  
+  import SchemaForm from 'svelte-jsonschema-form';
+
+  let configuration = fetchSchema();
+
+  async function fetchSchema() {
+    const url = "../configuration.json";    
+    const response = await fetch(url);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error(await response.text());
+    }
+  }
 </script>
 
 <main>
+  {#await configuration}
+    <p>Loading schema...</p>
+  {:then {schema}}
+    <SchemaForm {schema} />
+  {:catch error}
+    <div class="error">ERROR: {error.message}</div>
+  {/await}
 </main>
 
 <style lang="scss">
