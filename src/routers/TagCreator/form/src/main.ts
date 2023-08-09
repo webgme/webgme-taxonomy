a61d96777ed01a20c34b63ba72723bc44baf9a00
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-09 10:55:44 -0500

    setup initial TagCreator svelte project

---
+++ src/routers/TagCreator/form/src/main.ts
@@ -0,0 +1,11 @@
+// Don't import light/dark theme yet, since Search dashboard doesn't have dark theme enabled and it should match that.
+// import './theme/light.scss'
+// import './theme/dark.scss'
+import "./theme/_smui-theme.scss";
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
---
+++ src/routers/TagCreator/form/vite.config.ts
@@ -0,0 +1,20 @@
+import { createRequire } from "node:module";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import * as path from "path";
+
+const require = createRequire(import.meta.url);
+const smuiThemeDir = path.dirname(require.resolve("smui-theme/package.json"));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "",
+  plugins: [svelte()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: [path.join(smuiThemeDir, "fallback")],
+      },
+    },
+  },
+});
