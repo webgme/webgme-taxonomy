f0acea0a28f2295ac7c35e437a265f8529773d3f
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-10 17:11:44 -0500

    fix smui theme building

--- src/routers/TagCreator/form/src/main.ts
+++ src/routers/TagCreator/form/src/main.ts
@@ -1,7 +1,6 @@
 // Don't import light/dark theme yet, since Search dashboard doesn't have dark theme enabled and it should match that.
 // import './theme/light.scss'
 // import './theme/dark.scss'
-import "./theme/_smui-theme.scss";
 import App from "./App.svelte";
 
 const app = new App({
