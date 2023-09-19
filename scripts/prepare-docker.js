c890359f1c49a59fcd02cab3a351e47b90a07fec
Brian Broll <brian.broll@gmail.com>
2023-09-19 16:42:32 -0500

    Save updated package.json before building docker image

--- scripts/prepare-docker.js
+++ scripts/prepare-docker.js
@@ -1,5 +1,13 @@
 /// This script removes test dependencies from the docker image before building it.
 const pkgJson = require("../package.json");
+const fs = require("fs");
+const path = require("path");
+
 pkgJson.testDependencies.forEach((dep) => {
   delete pkgJson.devDependencies[dep];
 });
+
+fs.writeFileSync(
+  path.join(__dirname, "..", "package.json"),
+  JSON.stringify(pkgJson, null, 2),
+);
