0e0c874a4bf0a56fcf846aafff33fc2310b19ac7
Brian Broll <brian.broll@gmail.com>
2023-09-22 11:40:25 -0500

    Test PdpApi token usage for getting obs files (arg, fallback)

--- src/routers/Search/src/adapters/PDP/api.ts
+++ src/routers/Search/src/adapters/PDP/api.ts
@@ -267,7 +267,7 @@ export default class PdpApi implements PdpProvider {
 
     try {
       const response = await retry(async () => {
-        const response = await fetch(url, opts);
+        const response = await this._rawFetch(url, opts);
         const method = opts.method || "GET";
         console.log(
           `${method} ${url} ${JSON.stringify(opts)} ${response.status}`,
@@ -293,6 +293,10 @@ export default class PdpApi implements PdpProvider {
         );
       }
     }
+  }
+
+  private async _rawFetch(url: string, opts: RequestInit): Promise<Response> {
+    return await fetch(url, opts);
   }
 
   private async _fetchJson(
