c8591e33b031d98f15ce70971b605b6ad3712b25
Brian Broll <brian.broll@gmail.com>
2023-09-26 10:50:33 -0500

    Update ExportToJSONSchema to support content types

--- src/plugins/ExportToJSONSchema/ExportToJSONSchema.js
+++ src/plugins/ExportToJSONSchema/ExportToJSONSchema.js
@@ -26,15 +26,37 @@ define([
     }
 
     async main() {
+      const { onlyReleased } = this.getCurrentConfig();
       const exporter = new JSONSchemaExporter(this.core, this.META);
-      // TODO: Check if it is a taxonomy node. If so, export the children
-      // TODO: else, export the associated vocabularies
-      const { schema, uiSchema } = await exporter.getSchemas(this.activeNode);
+      const taxonomyName = this.core.getAttribute(this.activeNode, "name");
+      let schema, uiSchema;
+
+      if (this.core.isTypeOf(this.activeNode, this.META.Taxonomy)) {
+        const schemas = await exporter.getSchemas(
+          this.activeNode,
+          onlyReleased,
+        );
+        schema = schemas.schema;
+        uiSchema = schemas.uiSchema;
+      } else { // assume content type-like thing
+        const vocabContainer = (await this.core.loadChildren(this.activeNode))
+          .find((c) => this.core.isTypeOf(c, this.META.Vocabularies)) ||
+          this.META.Taxonomy; // fallback to default/base vocabularies if none specified
+
+        const vocabs = await this.core.loadChildren(vocabContainer);
+
+        const schemas = await exporter.getVocabSchemas(
+          vocabs,
+          taxonomyName,
+          onlyReleased,
+        );
+        schema = schemas.schema;
+        uiSchema = schemas.uiSchema;
+      }
       const files = {
         "schema.json": JSON.stringify(schema, null, 2),
         "uischema.json": JSON.stringify(uiSchema, null, 2),
       };
-      const taxonomyName = this.core.getAttribute(this.activeNode, "name");
       await this.addArtifact(taxonomyName, files);
       this.result.setSuccess(true);
     }
