733e5875dd2dd9951c04f6abb9d06a2d3d4507bf
Brian Broll <brian.broll@gmail.com>
2023-10-31 16:27:32 -0500

    Add version router for getting the server version

---
+++ src/routers/Version/Version.js
@@ -0,0 +1,57 @@
+/*globals define*/
+
+'use strict';
+
+// http://expressjs.com/en/guide/routing.html
+const pkgJson = require('../../../package-lock.json');
+var express = require('express'),
+    router = express.Router();
+
+/**
+ * Called when the server is created but before it starts to listening to incoming requests.
+ * N.B. gmeAuth, safeStorage and workerManager are not ready to use until the start function is called.
+ * (However inside an incoming request they are all ensured to have been initialized.)
+ *
+ * @param {object} middlewareOpts - Passed by the webgme server.
+ * @param {GmeConfig} middlewareOpts.gmeConfig - GME config parameters.
+ * @param {GmeLogger} middlewareOpts.logger - logger
+ * @param {function} middlewareOpts.ensureAuthenticated - Ensures the user is authenticated.
+ * @param {function} middlewareOpts.getUserId - If authenticated retrieves the userId from the request.
+ * @param {object} middlewareOpts.gmeAuth - Authorization module.
+ * @param {object} middlewareOpts.safeStorage - Accesses the storage and emits events (PROJECT_CREATED, COMMIT..).
+ * @param {object} middlewareOpts.workerManager - Spawns and keeps track of "worker" sub-processes.
+ */
+function initialize(middlewareOpts) {
+  const logger = middlewareOpts.logger.fork("Version");
+    logger.debug('initializing ...');
+
+    router.get('*', function (req, res/*, next*/) {
+        res.send(pkgJson.version);
+    });
+
+    logger.debug('ready');
+}
+
+/**
+ * Called before the server starts listening.
+ * @param {function} callback
+ */
+function start(callback) {
+    callback();
+}
+
+/**
+ * Called after the server stopped listening.
+ * @param {function} callback
+ */
+function stop(callback) {
+    callback();
+}
+
+
+module.exports = {
+    initialize: initialize,
+    router: router,
+    start: start,
+    stop: stop
+};
