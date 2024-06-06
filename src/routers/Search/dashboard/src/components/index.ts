80891c17c5d2e65c354d9dfcbfa591125ca3f230
Sean Wilson <contact@seanwilson.dev>
2024-06-06 13:56:45 -0500

    switch `AppendArtifactDialog` use to new component

--- src/routers/Search/dashboard/src/components/index.ts
+++ src/routers/Search/dashboard/src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as AppHeader } from "./AppHeader.svelte";
-export { default as AppendArtifactDialog } from "./AppendArtifactDialog.svelte";
+export { default as AppendArtifactDialog } from "./AppendArtifactDialog/index.svelte";
 export { default as ArtifactSetViewer } from "./ArtifactSetViewer.svelte";
 export { default as TaxonomyFilter } from "./TaxonomyFilter.svelte";
 export { default as TaxonomyFilterTree } from "./TaxonomyFilterTree.svelte";
