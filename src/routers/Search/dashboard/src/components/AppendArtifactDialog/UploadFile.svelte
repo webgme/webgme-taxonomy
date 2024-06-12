810b217185ef185c7753f58e2dc63825c7cc005c
Sean Wilson <contact@seanwilson.dev>
2024-06-12 13:05:40 -0500

    move over applicable styles for `UploadFile` component

--- src/routers/Search/dashboard/src/components/AppendArtifactDialog/UploadFile.svelte
+++ src/routers/Search/dashboard/src/components/AppendArtifactDialog/UploadFile.svelte
@@ -60,7 +60,40 @@
         <span class="upload-result warning">Cancelled</span>
       {/if}
     {:catch error}
-      <span style="upload-result error">Failed</span>
+      <span class="upload-result error">Failed</span>
     {/await}
   {/if}
 {/if}
+
+<style lang="scss">
+  .upload-file {
+    display: inline-flex;
+    width: 100%;
+    align-items: center;
+
+    .upload-file-name {
+      flex: 1;
+    }
+
+    :global(button) {
+      margin-bottom: 0;
+    }
+  }
+
+  .upload-result {
+    font-size: 0.8em;
+    font-style: italic;
+  }
+
+  .success {
+    color: var(--mdc-theme-success);
+  }
+
+  .warning {
+    color: var(--mdc-theme-warning);
+  }
+
+  .error {
+    color: var(--mdc-theme-error);
+  }
+</style>
