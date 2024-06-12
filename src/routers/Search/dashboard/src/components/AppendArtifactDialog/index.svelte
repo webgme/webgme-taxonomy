d550051b772e196158f71d15e77672147301bd0f
Sean Wilson <contact@seanwilson.dev>
2024-06-12 13:07:59 -0500

    flesh out new `AppendArtifactDialog`'s upload logic

--- src/routers/Search/dashboard/src/components/AppendArtifactDialog/index.svelte
+++ src/routers/Search/dashboard/src/components/AppendArtifactDialog/index.svelte
@@ -3,7 +3,8 @@
   A dialog for tagging and uploading content to a repository.
 -->
 <script lang="ts">
-  import type { Artifact, PopulatedRepo, UploadPromise } from "../../Storage";
+  import { createEventDispatcher, getContext } from "svelte";
+  import type { default as Storage, Artifact, PopulatedRepo, UploadPromise } from "../../Storage";
   import type { default as ContentType } from "../../ContentType";
 
   import Dialog, { Content, Title, Actions } from "@smui/dialog";
@@ -19,17 +20,22 @@
   /** The content ID to update. If null, then append instead */
   export let content: Artifact | null = null;
 
+  const dispatch = createEventDispatcher();
+  const storage: Storage = getContext("storage");
+
   let open = false;
   let title = "";
-  let artifactName = "";
+  let displayName = "";
   let tagging = true;
+  let tags: any;
   let uploads: UploadPromise[] | null = null;
-
+  let isReference = false;
   /** The files to append as an artifact to this artifact set. */
   let files: File[] = [];
 
   $: setOpen(repo != null);
   $: setNameAndTitle(repo, content);
+  $: updateIsReference(tags);
 
   function setOpen(value: boolean) {
     if (value !== open) open = value;
@@ -37,19 +43,69 @@
 
   function setNameAndTitle(repo: PopulatedRepo, content: Artifact | null) {
     const repoName = repo?.displayName ?? "";
-    artifactName = content?.displayName ?? repoName;
+    displayName = content?.displayName ?? repoName;
     title = (content != null) ? `Update ${content.displayName}`: `Append data to ${repoName}`;
   }
 
+  function updateIsReference(tags: any) {
+    const updatedIsReference = !!tags?.Base?.Location;
+    if (updatedIsReference != isReference) {
+      isReference = updatedIsReference;
+    }
+  }
+
+  function confirmUpload() {
+    if (!files.length && !isReference) {
+      const [title, prompt] = (content == null) ? 
+        [
+          'Upload without files?',
+          'Are you sure you want to upload without attaching any files?'
+        ] : [
+          'Reuse existing files?',
+          'Are you sure you only want to update the metadata?'
+        ];
+
+      return dispatch('confirm', { title, prompt, action: upload });
+    } else {
+      upload();
+    }
+  }
+
   async function upload() {
-    console.log("TODO: upload the files and metadata to the repo");
+    const metadata = { tags, displayName, taxonomyVersion: repo?.taxonomyVersion };
+    dispatch("upload");
+    try {
+      uploads = (content != null) ? 
+        await storage.updateArtifact(repo.id, content.id, metadata, files):
+        await storage.appendArtifact(repo, metadata, files);
+      await Promise.allSettled(uploads);
+      dispatch("complete");
+    } catch (err) {
+      console.error(err);
+      dispatchError(`Unable to upload: ${err.message}`);
+    } finally {
+      uploads = null;
+      open = false;
+    }
+  }
+
+  function dispatchError(error: string) {
+    return dispatch("error", { error });
+  }
+
+  function closeHandler(_e: CustomEvent<{ action: string }>) {
+    if (repo != null) repo = null;
+    files = [];
   }
 </script>
   
 <Dialog
   bind:open
+  scrimClickAction={uploads ? "" : null}
+  escapeKeyAction={uploads ? "" : null}
   aria-labelledby="upload-artifact-title"
   aria-describedby="upload-artifact-content"
+  on:SMUIDialog:closed={closeHandler}
 >
   <Title id="upload-artifact-title">{title}</Title>
   <Content id="upload-artifact-content">
@@ -96,7 +152,7 @@
           <Icon class="material-icons">arrow_forward</Icon>
         </Button>
       {:else}
-        <Button default  action={null} on:click={() => upload()}>
+        <Button default  action={null} on:click={confirmUpload}>
           <Label>Upload</Label>
           <Icon class="material-icons">upload</Icon>
         </Button>
