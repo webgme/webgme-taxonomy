b83956e466c329fd9fdb44a1c4a3bbb4c03ef347
Sean Wilson <seancwilson@gmail.com>
2024-10-15 12:30:16 -0500

    Add button to open uri tags (#578)
    * update svelte-jsonschema-form dependency
    
    for string input prepend/append support
    
    * add a dashboard uri button component
    
    * pass `UriTagButton` to `SchemaForm` if in readonly
    
    ---------
    
    Co-authored-by: Sean Wilson <contact@seanwilson.dev>

---
+++ src/routers/Search/dashboard/src/components/UriTagButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Fab, { Icon } from '@smui/fab';
+
+  export let data: string | null = null;
+  export let format: string | null = null;
+
+  function open() {
+    window.open(data ?? "");
+  }
+</script>
+
+{#if (format == "uri") && data}
+  <Fab color="primary" mini on:click={open} action={null}>
+    <Icon class="material-icons">open_in_new</Icon>
+  </Fab>
+{/if}
